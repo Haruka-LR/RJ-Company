5fc02f3b9850a9e92a4672da7c7238ec3869df2c
Haruka-LR <zomberoswaldo67@gmail.com>
2026-02-14 18:25:39 -0600

    Se modifico el diseño responsivo de moviles ya que tardaba en cargar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,37 @@
-/*El Layout es el componente que envuelve toda la aplicación y contiene elementos
-comunes como el Navbar y el Footer No es necesario colocar el elemento children 
-dentro de otro archivo ya que duplicaria los elementos, el layout es el unico
-con children y donde se importan los componentes como Navbar y Footer*/ 
 import type { ReactNode } from "react"
-import "./globals.css"; /*importa los estilos globales para toda la aplicación*/
-import Navbar from "@/components/Navbar"; /*importa el Navbar para su uso en el Layout*/
-import Footer from "@/components/Footer"; /*importa el footer para su uso en el Layout*/
-import type {Metadata} from "next";
+import "./globals.css"; 
+import Navbar from "@/components/Navbar"; 
+import Footer from "@/components/Footer"; 
+import type { Metadata } from "next";
 
-export const metadata: Metadata={
+export const metadata: Metadata = {
   title: "R-J COMPANY | Desarrollo Web y Aplicaciones ",
   description: "Desarrollo de paginas Web modernas, aplicaciones y sistemas a medida para negocios y emprendedores.",
 }
 
 export default function RootLayout({
-  children,}:{
-    children:ReactNode
-  }){
-  return(
-    <html lang="es">
-      <body>
-        <Navbar /> 
-        {children}
-        <Footer />
+  children, }: {
+    children: ReactNode
+  }) {
+  return (
+    <html lang="es" className="scroll-smooth">
+      <body className="bg-brand-bg">
+        {/* Agregamos 'flex flex-col min-h-screen' al contenedor principal.
+            Esto garantiza que el Footer siempre esté al fondo y 
+            no deje huecos blancos.
+        */}
+        <div className="relative flex flex-col min-h-screen w-full overflow-x-hidden">
+          <Navbar />
+          
+          {/* flex-grow hace que este div ocupe todo el espacio disponible,
+              empujando al footer hacia abajo incluso si hay poco contenido.
+          */}
+          <main className="flex-grow">
+            {children}
+          </main>
+          
+          <Footer />
+        </div>
       </body>
     </html>
   )
